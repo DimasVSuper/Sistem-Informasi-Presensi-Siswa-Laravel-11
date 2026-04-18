8692e36b7c32da49acdab6713e047c0d03017a67
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2026-04-18 13:51:46 +0700

    feat: add AuthController for user authentication and session management

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function login()
+    {
+        if (Auth::check()) {
+            return redirect()->route('dashboard.index');
+        }
+
+        return view('auth.login');
+    }
+
+    public function authenticate(AuthRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email atau Password salah.',
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('scan');
+    }
+}
